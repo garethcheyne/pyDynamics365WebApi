98556aa42de74e5714ad3cd2a40bde94025167cd
Gareth.Cheyne <gareth.cheyne@gmail.com>
2019-01-02 14:16:00 +1300

    Init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyDynamics365WebApi',
+    version='1.0.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Gareth.Cheyne',
+    author_email='gareth.cheyne@gmail.com',
+    description=''
+)
