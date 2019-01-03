e97728ab197c33e6ba04c3343b91d8323c9bd388
garethcheyne <gareth.cheyne@nz.harveynorman.com>
2019-01-03 15:12:44 +1300

    updated..

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='pyDynamics365WebApi',
-    version='1.0.0.0',
+    version='0.1.0.0',
     packages=[''],
     url='',
     license='',
-    author='Gareth.Cheyne',
+    author='Gareth Cheyne',
     author_email='gareth.cheyne@gmail.com',
     description=''
 )
